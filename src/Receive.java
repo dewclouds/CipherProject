2cbea6760906a1d9b821a86a992067db30307b63
FreezingClouds <raylenli2@gmail.com>
2016-05-10 13:03:17 -0700

    Structure creation

--- src/Receive.java
+++ src/Receive.java
@@ -1,2 +1,6 @@
-public class Receive {
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Receive implements Protocols {
 }
--- src/Transmit.java
+++ src/Transmit.java
@@ -1,2 +1,17 @@
-public class Transmit {
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Transmit implements Protocols {
+	public static void main (String[] args) throws Exception {
+		Scanner fileInput = new Scanner(new File("Input.txt"));
+		String stringInput = fileInput.nextLine();
+		byte[] original = stringInput.getBytes();
+		byte[] crypt = Protocols.encrypt(original);
+		byte[] result = Protocols.decrypt(crypt);
+		
+		System.out.println("Original: \t"+Arrays.toString(original));
+		System.out.println("Crypt: \t\t"+Arrays.toString(crypt));
+		System.out.println("Result: \t"+Arrays.toString(result));
+	}
 }
