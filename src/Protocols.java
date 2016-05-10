df916711cf92215945803b92d6b4b0b2c2d608d1
FreezingClouds <raylenli2@gmail.com>
2016-05-10 13:24:02 -0700

    Encrypt and decrypt methods

--- src/Protocols.java
+++ src/Protocols.java
@@ -1,4 +1,31 @@
 public interface Protocols {
-	public byte[] encrypt (byte[] input);
-	public byte[] decrypt (byte[] input);
+	public static void encrypt(long[] v, long[] k) {
+		long y = v[0];
+		long z = v[1];
+		long sum = 0;
+		final long delta = 0x9e3779b9;
+		int loops = 32;
+		while (loops-- > 0) {
+			sum += delta;
+			y += ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
+			z += ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+		}
+		v[0] = y;
+		v[1] = z;
+	}
+	public static void decrypt(long[] v, long[] k) {		
+		long y = v[0];
+		long z = v[1];
+		long sum;
+		final long delta = 0x9e3779b9;
+		int loops = 32;
+		sum = delta << 5;
+		while (loops-- > 0) {
+			z -= ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+			y -= ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
+			sum -= delta;
+		}
+		v[0] = y;
+		v[1] = z;
+	}
 }
--- src/Transmit.java
+++ src/Transmit.java
@@ -6,12 +6,26 @@ public class Transmit implements Protocols {
 	public static void main (String[] args) throws Exception {
 		Scanner fileInput = new Scanner(new File("Input.txt"));
 		String stringInput = fileInput.nextLine();
-		byte[] original = stringInput.getBytes();
-		byte[] crypt = Protocols.encrypt(original);
-		byte[] result = Protocols.decrypt(crypt);
 		
-		System.out.println("Original: \t"+Arrays.toString(original));
-		System.out.println("Crypt: \t\t"+Arrays.toString(crypt));
-		System.out.println("Result: \t"+Arrays.toString(result));
+		long[] v = {16,18};
+		long[] k = {4,4,4,4};
+		
+		System.out.println(Arrays.toString(v));
+		System.out.println(Arrays.toString(k));
+		
+		Protocols.encrypt(v, k);
+		
+		System.out.println(Arrays.toString(v));
+		System.out.println(Arrays.toString(k));
+		
+		Protocols.decrypt(v, k);
+		
+		System.out.println(Arrays.toString(v));
+		System.out.println(Arrays.toString(k));
+		//byte[] result = Protocols.decrypt(crypt);
+		
+		//System.out.println("Original: \t"+Arrays.toString(original));
+		//System.out.println("Crypt: \t\t"+Arrays.toString(crypt));
+		//System.out.println("Result: \t"+Arrays.toString(result));
 	}
 }
