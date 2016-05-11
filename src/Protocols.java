6ee91415a17da0ff8cb2ded135588573155e81e6
FreezingClouds <raylenli2@gmail.com>
2016-05-11 13:34:24 -0700

    Added key generating and string encryption

--- src/Protocols.java
+++ src/Protocols.java
@@ -1,31 +1,43 @@
 public interface Protocols {
-	public static void encrypt(long[] v, long[] k) {
-		long y = v[0];
-		long z = v[1];
-		long sum = 0;
-		final long delta = 0x9e3779b9;
-		int loops = 32;
-		while (loops-- > 0) {
-			sum += delta;
-			y += ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
-			z += ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+	public static long[] getKey(String key) {
+		byte[] keyBytes = key.getBytes();
+		long[] output = new long[4];
+		for (int i = 0, j = 0; j < 4; j++) {
+			output[j] = ((keyBytes[i++] & 0xff)) | ((keyBytes[i++] & 0xff) << 8) | ((keyBytes[i++] & 0xff) << 16) | ((keyBytes[i++] & 0xff) << 24);
 		}
-		v[0] = y;
-		v[1] = z;
+		return output;
 	}
-	public static void decrypt(long[] v, long[] k) {		
-		long y = v[0];
-		long z = v[1];
-		long sum;
-		final long delta = 0x9e3779b9;
-		int loops = 32;
-		sum = delta << 5;
-		while (loops-- > 0) {
-			z -= ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
-			y -= ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
-			sum -= delta;
+	public static void encrypt(long[][] v, long[] k) {
+		for (int i = 0; i < v.length; i++) {
+			long y = v[i][0];
+			long z = v[i][1];
+			long sum = 0;
+			final long delta = 0x9e3779b9;
+			int loops = 32;
+			while (loops-- > 0) {
+				sum += delta;
+				y += ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
+				z += ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+			}
+			v[i][0] = y;
+			v[i][1] = z;
 		}
-		v[0] = y;
-		v[1] = z;
+	}
+	public static void decrypt(long[][] v, long[] k) {
+		for (int i = 0; i < v.length; i++) {
+			long y = v[i][0];
+			long z = v[i][1];
+			long sum;
+			final long delta = 0x9e3779b9;
+			int loops = 32;
+			sum = delta << 5;
+			while (loops-- > 0) {
+				z -= ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+				y -= ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
+				sum -= delta;
+			}
+			v[i][0] = y;
+			v[i][1] = z;
+		}
 	}
 }
--- src/Transmit.java
+++ src/Transmit.java
@@ -5,27 +5,58 @@ import java.util.Scanner;
 public class Transmit implements Protocols {
 	public static void main (String[] args) throws Exception {
 		Scanner fileInput = new Scanner(new File("Input.txt"));
-		String stringInput = fileInput.nextLine();
+		String quote = fileInput.nextLine();
 		
-		long[] v = {16,18};
-		long[] k = {4,4,4,4};
+		String key = "0000111122223333";
+		long[] keyBytes = Protocols.getKey(key);
 		
-		System.out.println(Arrays.toString(v));
-		System.out.println(Arrays.toString(k));
 		
-		Protocols.encrypt(v, k);
+		long[][] v = new long[quote.length()/2][2];
+		for (int i = 0; i < v.length; i++) {
+			v[i][0] = quote.substring(i*2,i*2+1).getBytes()[0];
+			v[i][1] = quote.substring(i*2+1,i*2+2).getBytes()[0];
+		}
+		System.out.println("Original string:");
+		System.out.println(quote);
 		
-		System.out.println(Arrays.toString(v));
-		System.out.println(Arrays.toString(k));
+		System.out.println("Key:");
+		System.out.println(key);
+		System.out.println("Key data");
+		System.out.println(Arrays.toString(keyBytes));
+		System.out.println();
 		
-		Protocols.decrypt(v, k);
+		System.out.println("Original data:");
+		print2DArray(v);
 		
-		System.out.println(Arrays.toString(v));
-		System.out.println(Arrays.toString(k));
-		//byte[] result = Protocols.decrypt(crypt);
+		Protocols.encrypt(v, keyBytes);
+		System.out.println("Encrypted data:");
+		print2DArray(v);
 		
-		//System.out.println("Original: \t"+Arrays.toString(original));
-		//System.out.println("Crypt: \t\t"+Arrays.toString(crypt));
-		//System.out.println("Result: \t"+Arrays.toString(result));
+		String bleh = "";
+		for (int i = 0; i < v.length; i++) {
+			for (int j = 0; j < v[0].length; j++) {
+				bleh += (char)v[i][j];
+			}
+		}
+		System.out.println("Encrypted string:");
+		System.out.println(bleh);
+		
+		Protocols.decrypt(v, keyBytes);
+		System.out.println("Decrypted data:");
+		print2DArray(v);
+		
+		String result = "";
+		for (int i = 0; i < v.length; i++) {
+			for (int j = 0; j < v[0].length; j++) {
+				result += (char) v[i][j];
+			}
+		}
+		System.out.println("\nResultant string:");
+		System.out.println(result);
+	}
+	public static void print2DArray(long[][] n) {
+		for (int i = 0; i < n.length; i++) {
+			System.out.println(Arrays.toString(n[i]));
+		}
 	}
 }
