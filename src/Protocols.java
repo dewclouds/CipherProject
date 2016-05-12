51a2e360b7239647c07c5b921b6390f21ee295ab
WingofTech <NielsenN2150@sduhsd.lan>
2016-05-12 13:04:39 -0700

    Documented Protocols and Transmit
    Added comments giving explanations of cipher code. Protocols still
    requires some documentation possibly.

--- src/Protocols.java
+++ src/Protocols.java
@@ -7,7 +7,7 @@ public interface Protocols {
 		}
 		return output;
 	}
-	public static void encrypt(byte[][] v, byte[] k) {
+	public static void encrypt(byte[][] v, byte[] k) {	//Encryption method.
 		for (int i = 0; i < v.length; i++) {
 			byte y = v[i][0];
 			byte z = v[i][1];
@@ -16,14 +16,14 @@ public interface Protocols {
 			int loops = 32;
 			while (loops-- > 0) {
 				sum += delta;
-				y += ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
-				z += ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+				y += ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);	//Adds the following to 'y': Binary shifts 'z' by 4 to the left, adds k[0], binary or 'z' plus sum, binary or 'z' shifted by 5 to the right, and adds k[1].
+				z += ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);	//Adds the following to 'z': Binary shifts 'y' by 4 to the left, adds k[2], binary or 'y' plus sum, binary or 'y' shifted by 5 to the right, and adds k[3].
 			}
 			v[i][0] = y;
 			v[i][1] = z;
 		}
 	}
-	public static void decrypt(byte[][] v, byte[] k) {
+	public static void decrypt(byte[][] v, byte[] k) {	//Decryption method.
 		for (int i = 0; i < v.length; i++) {
 			byte y = v[i][0];
 			byte z = v[i][1];
@@ -32,8 +32,8 @@ public interface Protocols {
 			int loops = 32;
 			sum = delta << 5;
 			while (loops-- > 0) {
-				z -= ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
-				y -= ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
+				z -= ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);	//Subtracts the above encryption from 'z'.
+				y -= ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);	//Subtracts the above encryption from 'y'.
 				sum -= delta;
 			}
 			v[i][0] = y;
--- src/Transmit.java
+++ src/Transmit.java
@@ -2,35 +2,35 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Transmit implements Protocols {
+public class Transmit implements Protocols {	//Implements Protocols interface.
 	public static void main (String[] args) throws Exception {
-		Scanner fileInput = new Scanner(new File("Input.txt"));
+		Scanner fileInput = new Scanner(new File("Input.txt")); //Takes in String or .txt file for encryption.
 		String quote = fileInput.nextLine();
 		
-		String key = "0000111122223333";
+		String key = "0000111122223333";	//Sets encryption key.
 		byte[] keyBytes = Protocols.getKey(key);
 		
 		
 		byte[][] v = new byte[quote.length()/2][2];
-		for (int i = 0; i < v.length; i++) {
+		for (int i = 0; i < v.length; i++) {		//Converts quote into 2D array of byte values.
 			v[i][0] = quote.substring(i*2,i*2+1).getBytes()[0];
 			v[i][1] = quote.substring(i*2+1,i*2+2).getBytes()[0];
 		}
 		System.out.println("Original string:");
-		System.out.println(quote);
+		System.out.println(quote);		//Prints input string.
 		
 		System.out.println("Key:");
-		System.out.println(key);
+		System.out.println(key);		//Prints current encryption key.
 		System.out.println("Key data");
-		System.out.println(Arrays.toString(keyBytes));
+		System.out.println(Arrays.toString(keyBytes));		//Prints array of key.
 		System.out.println();
 		
 		System.out.println("Original data:");
-		print2DArray(v);
+		print2DArray(v);	//Prints original string's ASCII values per character.
 		
-		Protocols.encrypt(v, keyBytes);
+		Protocols.encrypt(v, keyBytes); //Encrypts data.
 		System.out.println("Encrypted data:");
-		print2DArray(v);
+		print2DArray(v);	//Prints original string data after encryption occurs.
 		
 		String bleh = "";
 		for (int i = 0; i < v.length; i++) {
@@ -39,11 +39,11 @@ public class Transmit implements Protocols {
 			}
 		}
 		System.out.println("Encrypted string:");
-		System.out.println(bleh);
+		System.out.println(bleh);	//WIP
 		
-		Protocols.decrypt(v, keyBytes);
+		Protocols.decrypt(v, keyBytes); //Decrypts data.
 		System.out.println("Decrypted data:");
-		print2DArray(v);
+		print2DArray(v);	//Prints data after decryption.
 		
 		String result = "";
 		for (int i = 0; i < v.length; i++) {
@@ -52,7 +52,7 @@ public class Transmit implements Protocols {
 			}
 		}
 		System.out.println("\nResultant string:");
-		System.out.println(result);
+		System.out.println(result); 	//Prints the resulting String of the decryption.
 	}
 	public static void print2DArray(byte[][] n) {
 		for (int i = 0; i < n.length; i++) {
